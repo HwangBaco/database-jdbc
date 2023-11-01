6db5ae9bb255715af01e51fe208b1c1cf728c9f4
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2023-11-01 11:09:16 +0900

    password init

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main extends JFrame {
     * 사용자에 따라 id / pw / dbname 입력
     * */
     public static final String ID = "root";
-    public static final String PW = "1570wpcjf!";
+    public static final String PW = "password";
     public static final String DB_NAME = "company";
     public static void main(String[] args) {
         new MainFrame();
