ad4e47db1de2f821d356c4d29d3c91d68ef1bfa7
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2023-11-04 21:47:57 +0900

    refactoring : apply batch query

--- src/JDBC/JDBCDeleteEmployeeData.java
+++ src/JDBC/JDBCDeleteEmployeeData.java
@@ -7,19 +7,28 @@ import java.util.Set;
 
 // 수민님이 작성해주시면 되는 class입니다!
 public class JDBCDeleteEmployeeData extends Component {
-    private StringBuilder sb;
     private final String baseDeleteClause = "DELETE FROM EMPLOYEE WHERE ";
 
     public void deleteEmployee(Set<String> ssnList, Connection conn) throws SQLException{
+        conn.setAutoCommit(false);
+        PreparedStatement pstmt;
+        StringBuilder sb = new StringBuilder();
+        String sql = sb.append(baseDeleteClause).append("ssn = ?;").toString();
+        pstmt = conn.prepareStatement(sql);
+
         try {
-            Statement stmt = conn.createStatement();
             for (String ssn : ssnList) {
-                sb = new StringBuilder();
-                String sql = sb.append(baseDeleteClause).append("ssn = ").append(ssn).append(";").toString();
-                stmt.execute(sql);
+                pstmt.setString(1, ssn);
+                pstmt.addBatch();
+                pstmt.clearParameters();
             }
+            pstmt.executeBatch();
+            conn.commit();
         } catch(Exception e) {
+            conn.rollback();
             e.printStackTrace();
+        } finally {
+            pstmt.close();
         }
     }
 }
